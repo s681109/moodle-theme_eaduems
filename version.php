fec6871bc13f9d262e8bc15987074f406ee4883b
Edson <50875405+s681109@users.noreply.github.com>
2026-08-14 14:00:34 -0400

    fix: use an icon for mobile navigation

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_eaduems';
-$plugin->version = 2026081401;
-$plugin->release = '1.2.0';
+$plugin->version = 2026081402;
+$plugin->release = '1.2.1';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2025092600;
 $plugin->dependencies = [
